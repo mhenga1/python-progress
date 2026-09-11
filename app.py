7252425c86efeb2f61862e2afc9f13ea88b8e81e
mhenga1 <waissworks@gmail.com>
2026-09-11 16:37:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,17 @@ for x in range(my_time ,0 , -1):
     time.sleep(1)
      
 print("TIME IS UP")
+
+#with hours now 
+import time 
+
+my_time = int (input("ENter the time in seconds: "))
+
+for x in range(my_time ,0 , -1):
+    seconds = x  % 60    
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600) % 60
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+     
+print("TIME IS UP")
