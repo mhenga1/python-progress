d6dd479dd747bcd011140979038ed490a4a6efaf
mhenga1 <waissworks@gmail.com>
2026-09-11 16:27:56 +0300

    Implement countdown timer in app.py
    Added a countdown timer feature that counts down from a user-defined time.

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ for x in range(0, my_time):
     time.sleep(1)
      
 print("TIME IS UP")
+
+#ANother version or reverse counting
+import time 
+
+my_time = int (input("ENter the time in seconds: "))
+
+for x in range(my_time ,0 , -1):
+    print(x)
+    time.sleep(1)
+     
+print("TIME IS UP")
