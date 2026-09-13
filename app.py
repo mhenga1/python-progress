8601170a5230ab8ee1e15e18a5a7da04683784e4
mhenga1 <waissworks@gmail.com>
2026-09-13 18:05:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,3 +32,15 @@ for x in range(my_time ,0 , -1):
     time.sleep(1)
      
 print("TIME IS UP")
+
+
+#NUMPAD PROGRAM 
+num_pad = ((1, 2, 3),
+           (4, 5, 6,),
+           (7, 8, 9,),
+           ("*", 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num,end=" ")
+    print()
